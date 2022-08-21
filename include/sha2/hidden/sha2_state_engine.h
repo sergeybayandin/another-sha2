8625f9f03bbba62e1da55707cc0399597e44813b
sergeybayandin <serezha-bayandin@mail.ru>
2022-08-21 21:45:28 +0700

    implemented Sha2StateEngine class

--- include/sha2/hidden/sha2_state_engine.h
+++ include/sha2/hidden/sha2_state_engine.h
@@ -1,14 +1,16 @@
 #ifndef SHA2_EXECUTOR_H
 #define SHA2_EXECUTOR_H
 
+#include <ranges>
+
 #include "common/consts.h"
 
 namespace sha2::hidden {
 
 template <
-    std::size_t rounds,
-    std::size_t chunk_size,
-    class       WordType
+    std::size_t               rounds,
+    std::size_t               chunk_size,
+    common::AvailableWordType WordType
 >
 class Sha2StateEngine final {
   public:
@@ -17,50 +19,121 @@ class Sha2StateEngine final {
       }
 
       void initialize_with(const std::initializer_list<WordType> &initializers) noexcept {
-          std::ranges::copy(initializers, h_.begin())
-          message_size_ = chunk_size_ = 0;
+          std::ranges::copy(initializers, states_.begin());
+          transformed_bytes_count_ = chunked_bytes_count_ = 0;
       }
 
       void transform_chunk() noexcept {
           std::array<WordType, rounds> words;
-          std::array<WordType, 8>      h;
-
+          std::array<WordType, 8>      states;
           
-          for (auto i : std::views::iota(16, rounds)) {
+          WordType t1, t2;
 
+          for (auto i : std::views::iota(0, 16)) {
+              words[i] = common::pack_as<WordType>(chunk().subspan(sizeof(WordType) * i, sizeof(WordType)));
           }
 
-          for (auto i : std::views::iota(0, rounds)) {
+          for (auto i : std::views::iota(std::size_t(16), rounds)) {
+              words[i] = words[i - 16] + common::s0(words[i - 15]) + words[i - 7]  +
+                         common::s1(words[i -  2]);
+          }
 
+          std::ranges::copy(states_, states.begin());
+
+          for (auto i : std::views::iota(std::size_t(0), rounds)) {
+              t2 = common::sig0(states[0]) + common::ma(states[0], states[1], states[2]);
+              
+              t1 = states[7] + 
+                   common::sig1(states[4]) + 
+                   common::ch(states[4], states[5], states[6]) + 
+                   common::consts<WordType, rounds>[i] +
+                   words[i];
+
+              states[7] = states[6];
+              states[6] = states[5];
+              states[5] = states[4];
+              states[4] = states[3] + t1;
+              states[3] = states[2];
+              states[2] = states[1];
+              states[1] = states[0];
+              states[0] = t1 + t2;
           }
 
           for (auto i : std::views::iota(0, 8)) {
-              h_[i] += h[i];
+              states_[i] += states[i];
           }
       }
 
-      inline auto h() noexcept {
-          return std::span(h_);
+      template <common::ContiguousBytesRange R>
+      void run(R &&range) noexcept {
+          auto begin = range.begin(), end = range.end();
+
+          if (chunked_bytes_count_) {
+              auto advance = std::copy_n(
+                  begin,
+                  std::min(chunk_size - chunked_bytes_count_, range.size()),
+                  unchunked().begin()
+              ) - unchunked().begin();
+              
+              begin += advance;
+              chunked_bytes_count_ += advance;
+
+              if (chunked_bytes_count_ == chunk_size) {           
+                  transform_chunk();
+                  transformed_bytes_count_ += chunked_bytes_count_;
+                  chunked_bytes_count_ = 0;
+              }
+          }
+
+          for (; end - begin > chunk_size; begin += chunk_size) {
+              std::copy_n(begin, chunk_size, chunk_.begin());            
+              transform_chunk();
+          }
+
+          chunked_bytes_count_      = end - begin;
+          transformed_bytes_count_ += begin - range.begin();
+
+          std::copy(begin, end, chunk_.begin());
+      }
+
+      inline auto states() noexcept {
+          return std::span(states_);
       }
 
       inline auto chunk() noexcept {
           return std::span(chunk_);
       }
 
-      inline std::size_t message_size() const noexcept {
-          return message_size_;
+      inline auto chunked() noexcept {
+          return chunk().first(chunked_bytes_count_);
       }
 
-      inline std::size_t chunk_size() const noexcept {
-          return chunk_size_;
+      inline auto unchunked() noexcept {
+          return chunk().last(chunk_size - chunked_bytes_count_);
+      }
+
+      inline auto transformed_bytes_count() const noexcept {
+          return transformed_bytes_count_;
       }
 
   private:
-      std::array<WordType, 8> h_;
+      std::array<WordType, 8> states_;
       std::array<std::byte, chunk_size> chunk_;
 
-      std::size_t message_size_, chunk_size_;
+      std::size_t transformed_bytes_count_, chunked_bytes_count_;
 };
+
+using Sha256StateEngine = Sha2StateEngine<
+    common::rounds<64>,
+    common::chunk_size<64>,
+    common::WordType<std::uint32_t>
+>;
+
+using Sha512StateEngine = Sha2StateEngine<
+    common::rounds<80>,
+    common::chunk_size<128>,
+    common::WordType<std::uint64_t>
+>;
 
 } // hidden
 
